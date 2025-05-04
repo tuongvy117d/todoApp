a2256ffd3193a0ea300c11ccba5241af9ef38b87
Anh Vu <danganhvu1998@gmail.com>
2025-05-04 10:15:11 +0900

    Implement dynamic username display in NewView component and add script setup

--- vue-express-ts-jwt/client/src/views/NewView.vue
+++ vue-express-ts-jwt/client/src/views/NewView.vue
@@ -1,10 +1,23 @@
-
 <template>
-  <h3>Hello alice</h3>
+<h3>Hello {{ username }}</h3>
 <form>
   <label><input type="checkbox" name="option1" /> Option 1</label><br>
   <label><input type="checkbox" name="option2" /> Option 2</label><br>
   <label><input type="checkbox" name="option3" /> Option 3</label><br>
- 
 </form>
+<h3>That is all, {{ username }}</h3>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const username = ref('Bate');
+    
+    return {
+      username
+    };
+  }
+});
+</script>
