044145d1b34fddd5e3947a3c6fc9d5565b138a62
Tuong Vy <tuongvy117d@gmail.com>
2025-06-15 10:01:42 +0900

    add todos

--- vue-express-ts-jwt/client/src/views/NewView.vue
+++ vue-express-ts-jwt/client/src/views/NewView.vue
@@ -1,7 +1,13 @@
 <template>
 <div class="todo-container">
-
-  
+ <span>hello{{ name }}</span>
+    <h1>{{ name }}'s todo list</h1>
+    <div v-for="todo in todos">
+      <p>{{ todo.todo }}</p>
+      <p>done:{{ todo.done }}</p>
+      <p>deadline:{{ todo.deadline }}</p>
+      <hr>
+    </div>
 </div>
 </template>
 
@@ -10,8 +16,22 @@ import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   setup() {
-    
-  }
+    var name = "Vy";
+    var todos = [
+      {todo: "Buy groceries (milk, eggs, bread)", done: true, id: 1, deadline: "2025-01-03"},
+      {todo: "Finish Vue.js project report", done: false, id: 2, deadline: "2025-01-05"},
+      {todo: "Call the dentist to reschedule appointment", done: false, id: 3, deadline: "2025-01-04"},
+      {todo: "Book flight tickets for vacation", done: false, id: 4, deadline: "2025-02-10"},
+      {todo: "Read 'Atomic Habits' book", done: false, id: 5, deadline: "2025-01-20"},
+      {todo: "Clean the garage", done: true, id: 6, deadline: "2025-01-02"},
+      {todo: "Prepare presentation for Monday meeting", done: false, id: 7, deadline: "2025-01-06"},
+    ]
+    return {
+      name,
+      todos,
+    };
+  },
+  
 });
 </script>
 
